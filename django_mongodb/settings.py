710088dc1cb8682c10d4d1187a025202002587e8
Apeksha Tiwary <tiwariapeksha7@gmail.com>
2023-04-02 14:37:26 +0530

    updates

--- django_mongodb/settings.py
+++ django_mongodb/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import os
-from mongoengine import connect
+# from mongoengine import connect
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g*ueinbrebz1w%=^ybmx3*ve0snjg*=%v1%fufgai-pi2_e9ec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
