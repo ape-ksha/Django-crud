16d7b0967ad61d311f81dc2f1149c9946e3b3067
Apeksha Tiwary <tiwariapeksha7@gmail.com>
2023-04-03 13:31:37 +0530

    deployment updates

--- django_mongodb/settings.py
+++ django_mongodb/settings.py
@@ -142,14 +142,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # MONGODB_DATABASES = {
 #     'default': {
