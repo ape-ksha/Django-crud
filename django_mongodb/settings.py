951b089188e296276919b87c1a46bc186560269b
Apeksha Tiwary <tiwariapeksha7@gmail.com>
2023-04-03 13:03:32 +0530

    deployment updates

--- django_mongodb/settings.py
+++ django_mongodb/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-crud.up.railway.app']
+ALLOWED_HOSTS = ['django-crud.up.railway.app',]
 
 
 # Application definition
