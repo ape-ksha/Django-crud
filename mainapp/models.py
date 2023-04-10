2dbff1bf1c2af58998fc285795185fb2f483f5bd
Apeksha Tiwary <tiwariapeksha7@gmail.com>
2023-04-10 11:21:47 +0530

    updated

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Shop(models.Model):
-    shop_id = models.CharField(max_length=100,primary_key=True)
+    # shop_id = models.CharField(max_length=100,primary_key=True)
     name = models.CharField(max_length=50)
     latitude = models.FloatField()
     longitude = models.FloatField()
